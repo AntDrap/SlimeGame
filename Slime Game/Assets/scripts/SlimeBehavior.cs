cf83b0fafffd839c51abd6f26767f155e016e5ac
AntDrap <adrapeau@mail.bradley.edu>
2024-01-16 00:10:53 -0600

    trying

--- Slime Game/Assets/scripts/SlimeBehavior.cs
+++ Slime Game/Assets/scripts/SlimeBehavior.cs
@@ -37,7 +37,7 @@ public class SlimeBehavior : MonoBehaviour
 
         if(!displaySlime)
         {
-            StartCoroutine(HoppingCoroutine());
+            //StartCoroutine(HoppingCoroutine());
         }
     }
 
